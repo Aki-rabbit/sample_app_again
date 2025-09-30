222b54bcbdecf72e3c5121d9ef3f30034ce4bb41
Aki Kakehi <197594452+Aki-rabbit@users.noreply.github.com>
2025-09-30 11:49:36 +0900

    aboutページとテスト

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,17 +1,34 @@
+# spec/requests/static_pages_spec.rb
 require 'rails_helper'
 
 RSpec.describe 'StaticPages', type: :request do
   describe 'GET /home' do
-    it 'returns http success' do
-      get '/static_pages/home'
-      expect(response).to have_http_status(:success)
+    context 'homeページにアクセスした場合' do
+      before { get static_pages_home_url }
+
+      it 'ステータスがsuccessになること' do
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 
   describe 'GET /help' do
-    it 'returns http success' do
-      get '/static_pages/help'
-      expect(response).to have_http_status(:success)
+    context 'helpページにアクセスした場合' do
+      before { get static_pages_help_url }
+
+      it 'ステータスがsuccessになること' do
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
+
+  describe 'GET /about' do
+    context 'aboutページにアクセスした場合' do
+      before { get static_pages_about_url }
+
+      it 'ステータスがsuccessになること' do
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 end
